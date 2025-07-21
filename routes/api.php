90d56e48db58459659eda14ae336f6b3e4e082b8
Willian Garcias <williangarcias10@gmail.com>
2025-07-20 22:49:35 -0300

    rota teste

--- routes/api.php
+++ routes/api.php
@@ -235,3 +235,9 @@ Route::prefix('historia')->group(function () {
         ]);
     });
 });
+
+Route::prefix('rota-teste')->group(function () {
+    Route::get('/camara', function (Request $request) {
+        return "Rota testada....";
+    });
+});
